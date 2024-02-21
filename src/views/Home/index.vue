296990b87bd65512791822c962e1cb4a64a96a85
BrenoGondimR <breno_gondim@hotmail.com>
2024-02-21 20:23:14 -0300

    cards acomodacoes

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -12,16 +12,35 @@
         </b-colxx>
       </b-row>
     </b-card>
+    <b-row class="mt-5">
+      <b-colxx lg="3" v-for="(acomodacao, index) in acomodacoesPatrocinadas" :key="index" style="">
+        <card-acomodacao
+            :image="acomodacao.imagem"
+            :title="acomodacao.title"
+            :price="acomodacao.price"
+            :description="acomodacao.description"
+        />
+      </b-colxx>
+    </b-row>
   </b-row>
 </template>
 
 <script>
 import BColxx from "@/components/Common/Colxx.vue";
 import InputAcomodacoes from "@/components/Common/InputAcomodacoes.vue";
+import CardAcomodacao from "@/components/Common/CardAcomodação.vue";
+
+//TODO TIRAR ESSES IMPORTS E PUXAR IMAGENS DO BANCO
+import escritorioMeirelesImg from '@/assets/img/escritorio1.jpg';
+import escritorio2Img from '@/assets/img/escritorio2.jpg';
+import escritorio3Img from '@/assets/img/escritorio3.jpg';
+import escritorio4Img from '@/assets/img/escritorio4.jpg';
+
 
 export default {
   name: "home",
   components: {
+    CardAcomodacao,
     BColxx,
     InputAcomodacoes
   },
@@ -29,6 +48,12 @@ export default {
     return {
       tituloCompleto: "Encontre o espaço perfeito para seus eventos e reuniões",
       tituloAnimado: "",
+      acomodacoesPatrocinadas: [
+        { imagem: escritorioMeirelesImg, title: 'Escritório Meireles', price: 'R$120/dia', description: 'Ótima localização...mais texto aqui' },
+        { imagem: escritorio2Img, title: 'Escritório Aldeota', price: 'R$120/dia', description: 'Ótima localização...mais texto aqui' },
+        { imagem: escritorio3Img, title: 'Escritório Iguatemi', price: 'R$120/dia', description: 'Ótima localização...mais texto aqui' },
+        { imagem: escritorio4Img, title: 'Coworking Cafeteria', price: 'R$120/dia', description: 'Ótima localização...mais texto aqui' },
+      ],
       typingSpeed: 75 // Velocidade de digitação em milissegundos
     };
   },
