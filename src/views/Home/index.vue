202208716a4f68bed772c174f580b4561c27d232
Leorlsa <leonardoandre@edu.unifor.br>
2024-02-27 20:21:54 -0300

    Novibreves

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -13,7 +13,7 @@
       </b-row>
     </b-card>
     <b-row class="mt-2">
-      <b-colxx class="mt-5" lg="3" v-for="(acomodacao, index) in acomodacoesPatrocinadas" :key="index" style="">
+      <b-colxx class="mt-5" lg="3" md="6" sm="12" v-for="(acomodacao, index) in acomodacoesPatrocinadas" :key="index" style="">
         <card-acomodacao
             :image="acomodacao.imagem"
             :title="acomodacao.title"
@@ -21,37 +21,37 @@
             :description="acomodacao.description"
         />
       </b-colxx>
-      <b-colxx lg="12" class="mt-4">
+      <b-colxx lg="12" class="mt-5">
         <h2 class="titles-primary" style="margin-bottom: 1rem">Como Funciona</h2>
         <p class="subtext-primary">ForHub é a maneira mais fácil de reservar um espaço de trabalho ou sala de reunião em todo o mundo.
           Com milhares de espaços de coworking e salas de reunião disponíveis para reserva sob demanda,
           você pode encontrar o local perfeito para o seu próximo evento, seja uma reunião de diretoria,
           conferência ou lançamento de produto.</p>
       </b-colxx>
-      <b-colxx class="mt-5" lg="4" v-for="(acomodacao, index) in detalhesForHub" :key="index" style="">
-        <card-acomodacao
-            :image="acomodacao.imagem"
-            :title="acomodacao.title"
-            :price="acomodacao.price"
-        />
-      </b-colxx>
+      <b-row style="padding: 0px 50px;">
+        <b-colxx class="mt-4" lg="4" v-for="(acomodacao, index) in detalhesForHub" :key="index" style="">
+          <card-acomodacao
+              :image="acomodacao.imagem"
+              :title="acomodacao.title"
+              :price="acomodacao.price"
+          />
+        </b-colxx>
+      </b-row>
       <b-colxx lg="12" class="mt-5">
         <Accordion :activeIndex="0">
           <AccordionTab header="Quais são os métodos de pagamento aceitos?">
-            <p class="m-0">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-              consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
-              laborum.
+            <p class="m-0 sub-text-accordion">
+              Aceitamos os seguintes métodos de pagamento: Cartão de crédito, boleto bancário, transferência eletrônica.
             </p>
           </AccordionTab>
           <AccordionTab header="Quais são os bairros que atendemos em Fortaleza?">
-            <p class="m-0">
+            <p class="m-0 sub-text-accordion">
               Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo
               enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Consectetur, adipisci velit, sed quia non numquam eius modi.
             </p>
           </AccordionTab>
           <AccordionTab header="Como faço para reservar um espaço de coworking?">
-            <p class="m-0">
+            <p class="m-0 sub-text-accordion">
               At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in
               culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus.
             </p>
@@ -74,6 +74,9 @@ import escritorioMeirelesImg from '@/assets/img/escritorio1.jpg';
 import escritorio2Img from '@/assets/img/escritorio2.jpg';
 import escritorio3Img from '@/assets/img/escritorio3.jpg';
 import escritorio4Img from '@/assets/img/escritorio4.jpg';
+import reservas from '@/assets/img/reservas.jpg';
+import estenderequipe from '@/assets/img/jobgroup.jpg';
+import eventos from '@/assets/img/eventos.jpg';
 
 
 export default {
@@ -96,9 +99,9 @@ export default {
         { imagem: escritorio4Img, title: 'Coworking Cafeteria', price: 'R$120/dia', description: 'Ótima localização...mais texto aqui' },
       ],
       detalhesForHub: [
-        { imagem: escritorioMeirelesImg, title: 'Reserve Sob Demanda', price: 'Reserve espaços de trabalho e salas de reunião em todo o mundo'},
-        { imagem: escritorio2Img, title: 'Estenda Sua Equipe', price: 'Economize tempo e dinheiro com um plano de membros que oferece acesso ilimitado a espaços de coworking ao redor do mundo'},
-        { imagem: escritorio3Img, title: 'Planeje Eventos', price: 'Encontre o local perfeito para o seu próximo evento, seja uma reunião de diretoria, conferência ou lançamento de produto'},
+        { imagem: reservas, title: 'Reserve Sob Demanda', price: 'Reserve espaços de trabalho e salas de reunião em todo o mundo'},
+        { imagem: estenderequipe, title: 'Estenda Sua Equipe', price: 'Economize tempo e dinheiro com um plano de membros que oferece acesso ilimitado a espaços de coworking ao redor do mundo'},
+        { imagem: eventos, title: 'Planeje Eventos', price: 'Encontre o local perfeito para o seu próximo evento, seja uma reunião de diretoria, conferência ou lançamento de produto'},
       ],
       typingSpeed: 75 // Velocidade de digitação em milissegundos
     };
@@ -126,5 +129,6 @@ export default {
 
 ::v-deep .p-accordion .p-accordion-header a {
   text-decoration: none !important;
+  color: black;
 }
 </style>
