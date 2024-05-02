94c62777e1ca21eb52fd81f5e9715a6b97ae34b2
lucaheset <lucaheset77@gmail.com>
2024-05-02 14:39:14 -0300

    Criada tela de Login para ForHub

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import {createRouter, createWebHashHistory, createWebHistory} from 'vue-router'
 import Home from '../views/Home/index.vue'
+import Login from '../views/Login/index.vue'
 
 const routes = [
   {
@@ -7,6 +8,12 @@ const routes = [
     name: 'home',
     component: Home
   },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login
+  },
+  
 ]
 
 const router = createRouter({
