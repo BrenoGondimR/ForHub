a67154127740d2899fd203c7c2002a2c9bc719e1
Leorlsa <leonardoandre@edu.unifor.br>
2024-06-06 19:39:55 -0300

    ajustando financeiro

--- src/views/Financeiro/index.vue
+++ src/views/Financeiro/index.vue
@@ -1,30 +1,55 @@
 <template>
-  <b-row class="main-flux">
+  <b-row class="main-flux" style="margin: 20px !important;">
     <b-colxx lg="12" class="mb-4">
       <h2 class="title-dash">Financeiro</h2>
     </b-colxx>
-    <b-colxx lg="9">
+    <b-colxx lg="12" class="mb-4">
+      <Button
+          label="Adicionar Finanças"
+          icon="pi pi-plus"
+          class="p-button-success rounded-button"
+          @click="showAddFinanceModal = true"
+      />
+    </b-colxx>
+    <b-colxx lg="9" md="8" sm="12">
       <b-row>
-        <b-colxx lg="3" v-for="card in cards" :key="card.title" class="mb-4">
+        <b-colxx lg="3" md="6" sm="12" v-for="card in cards" :key="card.title" class="mb-4">
           <CardDashboard
               :title="card.title"
               :value="card.value"
               :color="card.color"
           />
         </b-colxx>
-        <b-colxx lg="12" class="mb-4">
+        <b-colxx lg="12" md="12" sm="12" class="mb-4">
           <GradientLineChart title="Finances" :data="chartData" />
         </b-colxx>
-        <b-colxx lg="12" class="mb-4">
+        <b-colxx lg="12" md="12" sm="12" class="mb-4">
           <TransactionHistory :transactions="transactions" />
         </b-colxx>
       </b-row>
     </b-colxx>
-    <b-colxx lg="3">
-      <MyCard :card="cardDetails" />
+    <b-colxx lg="3" md="4" sm="12">
       <QuickTransaction :contacts="quickContacts" />
       <MyGoals :goals="goals" class="mt-4" />
     </b-colxx>
+
+    <Dialog header="Adicionar Finanças" :visible.sync="showAddFinanceModal" modal class="p-fluid" :style="{ width: '90vw', maxWidth: '500px' }" @hide="closeModal">
+      <div class="p-field">
+        <label for="finance-type">Tipo</label>
+        <Dropdown id="finance-type" v-model="financeType" :options="financeOptions" optionLabel="label" optionValue="value" placeholder="Selecione o tipo" />
+      </div>
+      <div class="p-field">
+        <label for="finance-value">Valor</label>
+        <span class="p-inputgroup">
+          <span class="p-inputgroup-addon">R$</span>
+          <InputNumber id="finance-value" v-model="financeValue" mode="currency" currency="BRL" locale="pt-BR" :minFractionDigits="2" />
+        </span>
+      </div>
+      <template #footer>
+        <Button label="Cancelar" icon="pi pi-times" class="p-button-text" @click="showAddFinanceModal = false" />
+        <Button label="Salvar" icon="pi pi-check" class="p-button-text" @click="saveFinance" />
+      </template>
+    </Dialog>
   </b-row>
 </template>
 
@@ -33,7 +58,6 @@ import BColxx from "@/components/Common/Colxx.vue";
 import CardDashboard from "@/components/Common/CardDashboard.vue";
 import GradientLineChart from "@/components/Common/GradientLineChart.vue";
 import TransactionHistory from "@/components/Common/TransactionHistory.vue";
-import MyCard from "@/components/Common/MyCard.vue";
 import QuickTransaction from "@/components/Common/QuickTransaction.vue";
 import MyGoals from "@/components/Common/MyGoals.vue";
 
@@ -43,10 +67,11 @@ export default {
     CardDashboard,
     GradientLineChart,
     TransactionHistory,
-    MyCard,
     QuickTransaction,
     MyGoals,
     BColxx,
+    Button,
+    Dropdown,
   },
   data() {
     return {
@@ -76,10 +101,6 @@ export default {
         { name: "Clement Stewart", type: "Compras", date: "March 27, 2022", amount: "-$241" },
         { name: "Jessica Johanne", type: "Outros", date: "March 25, 2022", amount: "$100" }
       ],
-      cardDetails: {
-        number: "5995 7474 1103 7513 0014",
-        expiry: "11/24"
-      },
       quickContacts: [
         { name: "Michael Jordan", image: "path/to/image1.jpg" },
         { name: "Edelyn Sandra", image: "path/to/image2.jpg" },
@@ -89,8 +110,29 @@ export default {
       goals: [
         { name: "New iMac", progress: 50 },
         { name: "New Macbook 14\"", progress: 60 }
+      ],
+      showAddFinanceModal: false,
+      financeType: null,
+      financeValue: null,
+      financeOptions: [
+        { label: "Renda", value: "renda" },
+        { label: "Despesa", value: "despesa" }
       ]
     };
+  },
+  methods: {
+    closeModal() {
+      this.showAddFinanceModal = false;
+    },
+    saveFinance() {
+      if (this.financeType && this.financeValue !== null) {
+        console.log('Finance Type:', this.financeType);
+        console.log('Finance Value:', this.financeValue);
+        this.showAddFinanceModal = false;
+      } else {
+        alert('Por favor, preencha todos os campos.');
+      }
+    }
   }
 };
 </script>
@@ -100,5 +142,9 @@ export default {
   font-family: "Poppins", sans-serif;
   font-weight: 600;
   color: black;
+}
+
+.rounded-button {
+  border-radius: 25px;
 }
 </style>
