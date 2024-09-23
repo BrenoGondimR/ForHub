dee75f436c48e9cefc67c901949a95fd990e28c5
Leorlsa <leonardoandre@edu.unifor.br>
2024-09-23 19:19:06 -0300

    ajustes

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,10 +1,6 @@
 let apisUrl;
 
-if (window.location.host === "https://for-hub.vercel.app/") {
-  apisUrl = 'https://apisforhub.onrender.com/'; // Usando 'https' após configurar SSL
-} else {
-  apisUrl = 'http://localhost:5001/'; // 'http' está correto apenas para desenvolvimento local
-}
+apisUrl = 'https://apisforhub.onrender.com/'; // Usando 'https' após configurar SSL
 
 export const createUserUrl = apisUrl + 'users';
 export const loginUrl = apisUrl + 'login';
